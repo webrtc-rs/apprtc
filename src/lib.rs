e5d0e93f5f8488f6cd1a2fca15c0490a45098bde
Rain Liu <y@liu.mx>
2026-07-02 18:10:05 -0700

    initial code for apprtc

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,5 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
+pub mod http;
+pub mod proto;
+pub mod room_table;
+pub mod webapp;
+pub mod ws;
