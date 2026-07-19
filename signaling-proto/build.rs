df6ab2775f78c24172916e50aec27ee864f9c34e
Rain Liu <y@liu.mx>
2026-07-19 16:27:44 -0700

    rename signaling.proto to signaling.v1.proto

--- signaling-proto/build.rs
+++ signaling-proto/build.rs
@@ -2,7 +2,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let protoc = protoc_bin_vendored::protoc_bin_path()?;
     let mut config = prost_build::Config::new();
     config.protoc_executable(protoc);
-    config.compile_protos(&["proto/signaling.proto"], &["proto"])?;
-    println!("cargo:rerun-if-changed=proto/signaling.proto");
+    config.compile_protos(&["proto/signaling.v1.proto"], &["proto"])?;
+    println!("cargo:rerun-if-changed=proto/signaling.v1.proto");
     Ok(())
 }
